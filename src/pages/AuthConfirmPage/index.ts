dca2ccaab2249a99482f03a90a6700dfac66d2bf
Hasan Ali Alsamman <hassan.alsamman2015@gmail.com>
2022-12-02 16:28:06 +0300

    add confirm user page

---
+++ src/pages/AuthConfirmPage/index.ts
@@ -0,0 +1,2 @@
+export { ConfirmEmailPageAsync as ConfirmEmailPage } from './ui/ConfirmEmailPage/ConfirmEmailPage.async';
+export { ConfirmUserPageAsync as ConfirmUserPage } from './ui/ConfirmUserPage/ConfirmUserPage.async';
---
+++ src/pages/AuthConfirmPage/ui/ConfirmUserPage/ConfirmUserPage.tsx
@@ -0,0 +1,17 @@
+import { ConfirmUser } from 'features/AuthConfirm';
+import { memo } from 'react';
+import { HStack } from 'shared/ui/Stack';
+import { Wrapper } from 'shared/ui/Wrapper/Wrapper';
+import cls from './ConfirmUserPage.module.scss';
+
+const ConfirmUserPage = memo(() => {
+	return (
+		<Wrapper>
+			<HStack className={cls.Wrapper} align="center" justify="center">
+				<ConfirmUser />
+			</HStack>
+		</Wrapper>
+	);
+});
+
+export default ConfirmUserPage;
