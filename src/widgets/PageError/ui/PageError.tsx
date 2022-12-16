b507959358f6105cfa2bcdf5fb4accb6a2638d4f
Roma <roman.pulich@softeq.com>
2022-12-16 16:53:53 +0300

    add reload button in error page

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,30 +1,27 @@
-// import { useTranslation } from "react-i18next";
-// import { Button } from "shared/ui/Buttons";
-// import { ButtonBgColor, ButtonSize } from "shared/ui/Buttons/types";
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { ButtonUi } from '@/shared/ui/Button/Button';
 import cls from './PageError.module.scss';
 
 export interface PageErrorProps {
 	className?: string;
 }
 export const PageError = ({ className }: PageErrorProps) => {
-	// const { t } = useTranslation("common");
-	// const realodPage = () => {
-	//   // eslint-disable-next-line no-restricted-globals
-	//   location.reload();
-	// };
+	const realodPage = () => {
+		// eslint-disable-next-line no-restricted-globals
+		location.reload();
+	};
 
 	return (
 		<div className={classNames(cls.PageError, {}, [className])}>
-			<p className={classNames(cls.info)}>произошла непредвиденная ошибка</p>
-			{/* <Button
-        sizes={ButtonSize.MEDIUM}
-        btnBg={ButtonBgColor.BLUE}
-        className={cls.btn}
-        onClick={realodPage}
-      >
-        {t("Обновить страницу")}
-      </Button> */}
+			<p className={classNames(cls.info)}>Somthing went wrang </p>
+			<ButtonUi
+				layOut="TextOnly"
+				theme="primary"
+				className={cls.btn}
+				onClick={realodPage}
+			>
+				Reload Page
+			</ButtonUi>
 		</div>
 	);
 };
